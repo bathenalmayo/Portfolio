a2d68b7d8891b6f4bbf8a1cfff2b3b43b14a64b1
בת חן אלמיהו <bathenalmayo@gmail.com>
2021-07-15 16:03:04 +0300

    saving

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from "react";
+
+function Header() {
+    /*mybe use 3 span with onClick event*/
+    return (
+        <header>
+            <h1> Bathen's Portfolio</h1>
+            <span>
+                <a href="" alt=""> ABOUT |</a>
+                <a href="" alt=""> PROJECTS |</a>
+                <a href="" alt=""> CONTACT</a>
+            </span>
+        </header>
+    )
+
+
+
+}
+export default Header;
