f0a99c591c6414aeee15e8748e9711844665e078
בת חן אלמיהו <bathenalmayo@gmail.com>
2021-07-22 13:46:02 +0300

    adding build

--- server/server.js
+++ server/server.js
@@ -2,14 +2,16 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, '..', 'build');
 
 
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(publicPath));
+//app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, '/build'));
+    res.sendFile(path.join(publicPath, 'index.html'));
  });
 
 app.listen(port, () => {
