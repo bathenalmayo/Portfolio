8971c201c0272827cb764a7e2e4d7289eca0b6b3
בת חן אלמיהו <bathenalmayo@gmail.com>
2021-07-22 13:31:34 +0300

    chancging the root

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'public');
 
 
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join(publicPath, '/build'));
  });
 
 app.listen(port, () => {
