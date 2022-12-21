bf3e5312a6bdeb14da33df85339ee016c362fd21
bathenalmayo <41523817+bathenalmayo@users.noreply.github.com>
2022-12-21 12:42:38 +0200

    change logos path

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,20 +9,20 @@ function Projects() {
             <div className="project_container">
                 <p>FULLSTACK WEB APPLICATION</p>
                 <a href="https://intense-dusk-47752.herokuapp.com/">
-                    <img src="\project_img.jpg" alt="my-project" />
+                    <img src="./project_img.jpg" alt="my-project" />
                 </a>
                 <div className="img_text">
                    
                 </div>
                 <div className="logo_container">
-                    <img src="\logo\html_icon.png" alt="logo" />
-                    <img src="\logo\css_icon.png" alt="logo" />
-                    <img src="\logo\js_icon.png" alt="logo" />
-                    <img src="\logo\bootstrap_logo_icon.png" alt="logo" />
-                    <img src="\logo\nodejs_logo_icon.png" alt="logo" />
-                    <img src="\logo\heroku_icon.png" alt="logo" />
-                    <img src="\logo\mongodb_logo_icon.png" alt="logo" />
-                    <img src="\logo\express_logo_icon.png" alt="logo" />
+                    <img src="./logo/html_icon.png" alt="logo" />
+                    <img src="./logo/css_icon.png" alt="logo" />
+                    <img src="./logo/js_icon.png" alt="logo" />
+                    <img src="./logo/bootstrap_logo_icon.png" alt="logo" />
+                    <img src="./logo/nodejs_logo_icon.png" alt="logo" />
+                    <img src="./logo/heroku_icon.png" alt="logo" />
+                    <img src="./logo/mongodb_logo_icon.png" alt="logo" />
+                    <img src="./logo/express_logo_icon.png" alt="logo" />
                 </div>
 
             </div>
